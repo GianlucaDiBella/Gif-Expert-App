01ad025a6aa2a825d3a9c718c550d27211c485a2
Gianluca Di Bella <gianluca.dib99@gmail.com>
2020-07-24 16:36:07 -0400

    First commit

---
+++ docs/precache-manifest.3d9c7cbb18055b6ddc51ce2646539734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63987f0490b9c94b7e8aa9c908ee992e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7616906b9fd837645fa8",
+    "url": "/static/css/main.f176a647.chunk.css"
+  },
+  {
+    "revision": "63ef298db94e50cc6fc1",
+    "url": "/static/js/2.909d688d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.909d688d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7616906b9fd837645fa8",
+    "url": "/static/js/main.84ddb6f1.chunk.js"
+  },
+  {
+    "revision": "603658e17899ebee4bd8",
+    "url": "/static/js/runtime-main.39ca27c7.js"
+  }
+]);
---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+    // , 'Spiderman', 'Buzz Lightyear', 'Rick And Morty'
+    const [categories, setCategories] = useState(['Bob Esponja'])
+
+    // const handleAdd = () => {
+    // //   setCategories(categories.concat('Frozono'))
+    // //   setCategories( cats => [...cats, 'Frozono'])
+    //      setCategories(['Frozono',...categories])
+    // }
+
+    return(
+    <>
+    <h2>GifExpertApp</h2>
+    <AddCategory setCategories={ setCategories }/>
+    <hr />
+    <ol>
+        {
+            categories.map( (category) => (
+               <GifGrid 
+                    category={category}
+                    key={category}/>
+            ))
+        }
+    
+    </ol>
+
+    </>
+)
+
+}
